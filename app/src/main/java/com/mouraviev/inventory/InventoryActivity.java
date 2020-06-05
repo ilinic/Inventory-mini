8662d094931e3ad4601ab42525827ff38075a6e4
ilinic <ilinic@mail.ru>
2020-06-05 15:14:37 +0300

    c

---
+++ app/src/main/java/com/mouraviev/inventory/InventoryActivity.java
@@ -0,0 +1,13 @@
+package com.mouraviev.inventory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
